2852f67bf1b04a49dd814bd9bb461ef1de67de49
unknown <lfrackowiak4@gmail.com>
2022-05-18 18:20:24 +0200

    teraz powinno dzialac

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 class Pracownik:
   def __init__(self, imie, wynagrodzenie):
     self.imie_pracownika=str(imie)
@@ -7,28 +5,39 @@ class Pracownik:
 
 
   def wynagrodzenie_netto(self)->float:
+      
+      
+    podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne=float(self.wynagrodzenie_brutto)
 
-      podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne=self.wynagrodzenie_brutto
+    podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne,2)
 
-      skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0976,2) + round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.015,2) + round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0245,2)
+    wynagrodzenie_brutto=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne,2)
+
+    skladka_emerytalna=podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0976
+
+    skladka_rentowa=podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.015
+
+    skladka_chorobowa=podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0245
+
+    skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika=round(skladka_emerytalna,2)+round(skladka_rentowa,2)+round(skladka_chorobowa,2)
             
-      podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne,2)-round(skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,2)
+    podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,2)
       
-      skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia = round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.09,2)
+    skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia=round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.09,2)
       
-      skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku = round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.075,2)
+    skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku=round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.0775,2)
       
-      koszty_uzyskania_przychodu=111.25
+    koszty_uzyskania_przychodu=111.25
           
-      podstawa_obliczenia_zaliczki_podatek_dochodowy=round(self.wynagrodzenie_brutto-koszty_uzyskania_przychodu,2)-round(skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,2)
+    podstawa_obliczenia_zaliczki_podatek_dochodowy=round(self.wynagrodzenie_brutto-koszty_uzyskania_przychodu-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,0)
 
-      zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej=round(round(podstawa_obliczenia_zaliczki_podatek_dochodowy*0.18),2) - 46.33
+    zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej=round(podstawa_obliczenia_zaliczki_podatek_dochodowy*0.18,2) - 46.33
         
-      zaliczka_na_podatek_dochodowy_do_pobrania=round(zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej-skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku,0)
+    zaliczka_na_podatek_dochodowy_do_pobrania=round(zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej-skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku,0)
       
-      netto=round(self.wynagrodzenie_brutto-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika-skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia-zaliczka_na_podatek_dochodowy_do_pobrania,2)
+    netto=round(self.wynagrodzenie_brutto-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika-skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia-zaliczka_na_podatek_dochodowy_do_pobrania,2)
 
-      return netto
+    return netto
 
   def skladki_pracodawcy(self)->float:
     
@@ -69,28 +78,39 @@ class Pracownik:
 
 
   def wynagrodzenie_netto(self)->float:
+      
+      
+    podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne=float(self.wynagrodzenie_brutto)
 
-      podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne=self.wynagrodzenie_brutto
+    podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne,2)
 
-      skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0976,2) + round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.015,2) + round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0245,2)
+    wynagrodzenie_brutto=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne,2)
+
+    skladka_emerytalna=podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0976
+
+    skladka_rentowa=podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.015
+
+    skladka_chorobowa=podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne*0.0245
+
+    skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika=round(skladka_emerytalna,2)+round(skladka_rentowa,2)+round(skladka_chorobowa,2)
             
-      podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne,2)-round(skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,2)
+    podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne=round(podstawa_wymiaru_skladek_na_ubezpieczenia_spoleczne-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,2)
       
-      skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia = round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.09,2)
+    skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia=round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.09,2)
       
-      skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku = round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.075,2)
+    skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku=round(podstawa_wymiaru_skladki_na_ubezpieczenie_zdrowotne*0.0775,2)
       
-      koszty_uzyskania_przychodu=111.25
+    koszty_uzyskania_przychodu=111.25
           
-      podstawa_obliczenia_zaliczki_podatek_dochodowy=round(self.wynagrodzenie_brutto-koszty_uzyskania_przychodu,2)-round(skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,2)
+    podstawa_obliczenia_zaliczki_podatek_dochodowy=round(self.wynagrodzenie_brutto-koszty_uzyskania_przychodu-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika,0)
 
-      zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej=round(round(podstawa_obliczenia_zaliczki_podatek_dochodowy*0.18),2) - 46.33
+    zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej=round(podstawa_obliczenia_zaliczki_podatek_dochodowy*0.18,2) - 46.33
         
-      zaliczka_na_podatek_dochodowy_do_pobrania=round(zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej-skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku,0)
+    zaliczka_na_podatek_dochodowy_do_pobrania=round(zaliczka_podatek_dochodowy_przed_odliczeniem_skladki_zdrowotnej-skladka_na_ubezpieczenie_zdrowotne_do_odliczenia_od_podatku,0)
       
-      netto=round(self.wynagrodzenie_brutto-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika-skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia-zaliczka_na_podatek_dochodowy_do_pobrania,2)
+    netto=round(self.wynagrodzenie_brutto-skladki_na_ubezpieczenia_spoleczne_finansowane_przez_pracownika-skladka_na_ubezpieczenie_zdrowotne_do_pobrania_z_wynagrodzenia-zaliczka_na_podatek_dochodowy_do_pobrania,2)
 
-      return netto
+    return netto
 
   def skladki_pracodawcy(self)->float:
     
@@ -122,3 +142,5 @@ for index in range(0,i):
   print(pracownicy[index][0]+ " " +str(format(pracownik_index.wynagrodzenie_netto(),'.2f'))+ " " +str(pracownik_index.skladki_pracodawcy())+ " " +str(pracownik_index.koszty_pracodawcy()))
 
 print(float(format(laczny_koszt_pracodawcy,'2f')))
+
+
